663b46db2b5dbf55c53c0b986364455db9efea6c
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-01-16 02:14:18 +0900

    feat : 로그인 콜백 화면 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,7 @@
 import React, { useEffect } from 'react'
 const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&scope=${process.env.REACT_APP_GOOGLE_SCOPE1} ${process.env.REACT_APP_GOOGLE_SCOPE2}&response_type=code`;
 function Home() {
-    useEffect(()=>{
-
-        const code = new URL(document.location.toString()).searchParams.get(
-            'code',
-        ) as string;
-        console.log( "인가코드 : ",code);
-        console.log( "한번 사용한 인가코드는 다시 사용못하므로 재발급 받아야함");
-    },[]);
+   
     console.log(GOOGLE_AUTH_URL);
   return (
     <div> <button>
