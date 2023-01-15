ea36fd014bb024c1ffa52c189684bb9b5952ca4a
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-01-16 02:08:18 +0900

    feat : 구글 로그인 인가코드 받기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+<Router>
+				<Suspense fallback={<div>Loading..</div>}>
+					<Routes>
+					<Route path="/" element={<Home />} />
+						<Route path="/oauth2/callback" element={<Home />} />
+
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</Suspense>
+			</Router>
+      
     </div>
   );
 }
 
 export default App;
+/*
+
+
+
+
+         */
