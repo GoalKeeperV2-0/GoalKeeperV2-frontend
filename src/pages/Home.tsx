bdfaaf90fcf53c0a6330a73b22f31cda8e9ef727
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-01-17 05:41:37 +0900

    chore : tailwindCSS 설치

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
     console.log(GOOGLE_AUTH_URL);
     console.log(process.env.REACT_APP_GOOGLE_REDIRECT_URI);
   return (
-    <div> <button>
+    <div> <button className='bg-blue-400'>
     <a href={GOOGLE_AUTH_URL}>구글로그인버튼</a>
    </button>
 </div>
