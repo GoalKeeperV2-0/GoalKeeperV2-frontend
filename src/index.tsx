dcf4f0d6c1e64e3e2b27dada10b7d1d3b386afa0
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-01-20 03:36:18 +0900

    design : global css update

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'styles/index.css';
+import 'styles/tailwind.css';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
