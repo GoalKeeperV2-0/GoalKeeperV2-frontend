3f4c46d48432c4f59c4f4fff658e9962e5bb1263
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-01-27 15:49:35 +0900

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import LoginView from 'app.features/Auth/views/LoginView';
 
 function Login() {
-	return <LoginView onSubmit={() => null} error="" />;
+	return <div />;
 }
 
 export default Login;
