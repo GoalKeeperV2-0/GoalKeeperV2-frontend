e1c17ab0133b82690bf2431d0e15a60fe650c88b
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-13 15:29:25 +0900

    feat : goal detail. 골 제목, 내용 부분 길어지는 경우 처리

--- src/app.features/GoalManage/modalContents/DetailGoal.tsx
+++ src/app.features/GoalManage/modalContents/DetailGoal.tsx
@@ -90,14 +90,17 @@ function DetailGoal({ goal }: Props) {
 	return (
 		<div className="space-y-[3.2rem]">
 			<div className="flex justify-between max-h-[9.5rem]">
-				<div className="max-w-[19rem] ">
-					<p className="pc:text-body7-pc  text-ellipsis max-h-full  overflow-hidden ">
+				<div className="max-w-[19rem] w-[19rem] overflow-hidden  whitespace-normal">
+					<p className="pc:text-body7-pc   ">
 						{goal.title.slice(0, 26)}
 						{goal.title.length > 26 && '...'}
 					</p>
 				</div>
 				<div className="w-[46.4rem]  flex flex-col justify-between">
-					<p className="whitespace-pre-wrap h-[4.4rem] w-full truncate pc:text-body4-pc">{goal.content}</p>
+					<p className="whitespace-pre-wrap h-[4.4rem] w-full truncate pc:text-body4-pc">
+						{goal.content.slice(0, 56)}
+						{goal.content.length > 56 && '...'}
+					</p>
 
 					<div className="flex justify-between items-center">
 						<div className="flex space-x-[0.8rem]">
