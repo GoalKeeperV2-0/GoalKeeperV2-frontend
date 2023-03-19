e3c45208305c49dcf7f86e06e12d1c1d63cc59ec
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-20 00:47:41 +0900

    fix : goldetail 업로드 영역  인증내용 없으면 required mark 표시

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import BaseLayout from 'app.components/BaseLayout';
 import HomeScreen from 'app.features/Home/screens/HomeScreen';
 import { useCertList } from 'app.modules/hooks/useCertList';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function HomePage() {
 	const { data: certs } = useCertList(0);
 	return (
 		<BaseLayout>
-			<HomeScreen certs={certs?.content?.slice(0, 6)} />
+			<HomeScreen certs={certs?.certificationResponses?.content?.slice(0, 6)} />
 		</BaseLayout>
 	);
 }
